bba3e93940e48b493c6fce486a2f1c9191727c8d
Dario George <edu.dariogeorge21@gmail.com>
2026-03-03 22:11:27 +0530

    Update event description for Blooming Roses 26 with new dates and location

--- src/app/about/metadata.js
+++ src/app/about/metadata.js
@@ -1,4 +1,4 @@
 export const metadata = {
   title: "About | Blooming Roses",
-  description: "Join Blooming Roses 25, an exciting event for teens from April 24-27 at Tabore Kodumpidi, Pala.",
+  description: "Join Blooming Roses 26, an exciting event for teens from April 26-30 at Chavara Public School, Pala.",
 }
